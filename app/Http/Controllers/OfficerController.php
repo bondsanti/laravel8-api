3347a95a6d1fd1e9e6d820527f50b1a009b0e8d2
Santi Dev <santi.chpy@gmail.com>
2022-07-11 23:20:56 +0700

    upload_picture64

--- app/Http/Controllers/OfficerController.php
+++ app/Http/Controllers/OfficerController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Officer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class OfficerController extends Controller
 {
@@ -29,7 +31,52 @@ class OfficerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            DB::beginTransaction();
+
+            $officer = new Officer();
+
+            if ($request->has('picture')) { //เช็คค่าว่ามีข้อมูลมาหรือไม่
+                $base64_image = $request->picture;
+                @list($type, $file_data) = explode(';', $base64_image);
+                @list(, $file_data) = explode(',', $file_data);
+
+                $new_filename = uniqid() . '.png';
+
+                if ($file_data != "") {
+                    Storage::disk('public')->put('uploads/officer/'.$new_filename, base64_decode($file_data));
+                }
+                $officer->firstname = $request->firstname;
+                $officer->lastname = $request->lastname;
+                $officer->dob = $request->dob;
+                $officer->salary = $request->salary;
+                $officer->user_id = $request->user_id;
+                $officer->department_id = $request->department_id;
+                $officer->picture = $new_filename;
+
+            } else {
+                $officer->firstname = $request->firstname;
+                $officer->lastname = $request->lastname;
+                $officer->dob = $request->dob;
+                $officer->salary = $request->salary;
+                $officer->user_id = $request->user_id;
+                $officer->department_id = $request->department_id;
+            }
+
+            $officer->save();
+            DB::commit();
+
+            return response()->json([
+                'message' => 'เพิ่มข้อมูลพนักงานเรียบร้อย'
+            ], 201);
+
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return response()->json([
+                'message' => 'เกิดข้อผิดพลาดในการเพิ่มข้อมูล',
+                'system message' => $th->getMessage()
+            ], 400);
+        }
     }
 
     /**
